e1a590445d8aae1339c61cd345a3e4735ce2b529
Takeshi Kato <takeshi.kato@creativegear.co.jp>
2024-05-26 18:06:09 +0900

    修正

--- next/src/app/api/graphql/route.ts
+++ next/src/app/api/graphql/route.ts
@@ -2,12 +2,10 @@ import { readFileSync } from "fs";
 import { join } from "path";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { ApolloServer } from "@apollo/server";
-import { Resolvers } from "../../../../apollo/__generated__/server/resolvers-types";
+import { Resolvers } from "../../../../graphql/__generated__/server/resolvers-types";
 
-const typeDefs = readFileSync(
-  join(process.cwd(), "apollo/documents/schema.gql"),
-  "utf-8"
-);
+const schemaFilePath = join(process.cwd(), "graphql/documents/schema.gql");
+const typeDefs = readFileSync(schemaFilePath, "utf-8");
 
 const resolvers: Resolvers = {
   Query: {
@@ -23,7 +21,6 @@ const resolvers: Resolvers = {
 };
 
 const apolloServer = new ApolloServer<Resolvers>({ typeDefs, resolvers });
-
 const handler = startServerAndCreateNextHandler(apolloServer);
 
 export { handler as GET, handler as POST };
